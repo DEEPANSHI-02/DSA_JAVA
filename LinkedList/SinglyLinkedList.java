50020d6a05744fbbbd42190b8aff15fa5ee3727a
Deepanshi <deepanshikadian@Deepanshis-MacBook-Air.local>
2026-09-17 01:11:37 +0530

    add search

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -1,6 +1,6 @@
 public class SinglyLinkedList {
 
-    static Class Node {
+    static class Node {
         int data;
         Node next;
 
@@ -24,7 +24,7 @@ public class SinglyLinkedList {
 
     // INSERTION 
     // iNSERT AT BEGINNING
-    public void insertAthead(int data){
+    public void insertAtHead(int data){
 
         Node newNode = new Node(data);
         
@@ -50,7 +50,7 @@ public class SinglyLinkedList {
             tail = newNode;
         }else{
             tail.next = newNode;
-            newNode.next = null;
+            tail = newNode;
         }
         size++;
     }
@@ -59,7 +59,7 @@ public class SinglyLinkedList {
     public void insertAtPosition(int data, int position){
         
         if(position < 1 || position > size+1){
-            System.out.println("insertion is not possible at this position")
+            System.out.println("insertion is not possible at this position");
             return ;
         } 
         if(position == 1){
@@ -88,9 +88,98 @@ public class SinglyLinkedList {
 
     // TRAVERSAL
 
+    public void printList(){
+        Node temp = head;
+
+        while(temp != null){
+            System.out.print(temp.data + "->");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    // utility function
     
+    public int getSize(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public int getHead(){
+
+        if(head == null){
+            return -1;
+        } else{
+            return head.data;
+        }
+    }
+
+    public int getTail(){
+
+        if(tail == null){
+            return -1;
+        } else{
+            return tail.data;
+        }
+    }
+
+    public void clear(){
+
+    }
+
+    public boolean search(int target){
+
+        Node temp = head;
+        while(temp != null){
+            if(temp.data == target) {
+                return true;
+            }else {
+                temp = temp.next;
+            }
+        }
+        return false;
+    }
 
     public static void main(String[] args){
 
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        if(list.isEmpty()){
+            System.out.println("List is empty");
+        }
+
+        System.out.println("size of LL: "+ list.getSize());
+
+        list.insertAtHead(10);
+        list.printList();
+
+        list.insertAtHead(20);
+        list.printList();
+
+        list.insertAtHead(30);
+        list.printList();
+
+        list.insertAtTail(100);
+        list.printList();
+
+        list.insertAtTail(110);
+        list.printList();
+
+        list.insertAtTail(120);
+        list.printList();
+
+        list.insertAtPosition(22, 1);
+        list.printList();
+
+        list.insertAtPosition(200, 8);
+        list.printList();
+
+        list.insertAtPosition(500,5);
+        list.printList();
+
+        System.out.print("Found or not:" + list.search(700));
     }
 }
